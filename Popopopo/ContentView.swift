2b7deeea132101d081254203f4ceb3ffb4ab071f
Eduardo Lopes de Carvalho <eduardo.lopes.c@gmail.com>
2021-02-04 18:00:47 -0300

    Initial Commit

---
+++ Popopopo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Popopopo
+//
+//  Created by Eduardo Lopes de Carvalho on 04/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Popopopo/PopopopoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopopopoApp.swift
+//  Popopopo
+//
+//  Created by Eduardo Lopes de Carvalho on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PopopopoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
